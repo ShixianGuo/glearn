cc2dc075c15e084aa99d05bc34eb1d83eb74e643
ShixianGuo <729841861@qq.com>
2020-04-07 23:38:22 +0800

    add.md

---
+++ src/eth_device.c
@@ -0,0 +1,32 @@
+
+#include <linux/module.h>
+#include <linux/netdevice.h>
+#include <linux/etherdevice.h>
+
+#include <linux/inetdevice.h>
+
+
+
+
+static int	nic_open(struct net_device *dev){
+
+}
+
+static int nic_stop(struct net_device *dev){
+
+}
+
+
+static int nic_change_mtu(struct net_device *dev, int new_mtu) {
+	struct nic_priv *priv = netdev_priv(dev);
+	netif_info(priv, drv, dev, "%s(#%d), priv:%p, mtu%d\n",
+                __func__, __LINE__, priv, new_mtu);
+
+	return eth_change_mtu(dev, new_mtu);
+}
+
+static struct net_device*nic_dev;
+static struct net_device_ops * nic_dev_ops={
+    .ndo_open = nic_open,
+	.ndo_stop = nic_stop
+};
