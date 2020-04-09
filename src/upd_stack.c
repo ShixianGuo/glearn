243f0fbed030d9086ed51ebd024df6b3f20fd724
ShixianGuo <729841861@qq.com>
2020-04-09 20:38:12 +0800

    add.md

--- src/upd_stack.c
+++ src/upd_stack.c
@@ -1,156 +1,218 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<stirng.h>
-#include<stdlib.h>
-
-#include<sys/poll.h>
-#include<arpa/inet.h>
-
-#define NETMAP_WITH_LIBS
-
-#include<net/netmap_user.h>
 
 
-  
-#define PROTO_IP  0x0800
-#define PROTO_ARP 0x0806
-#define PROTO_UDP  17
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <sys/poll.h>
+#include <arpa/inet.h>
+
+#define NETMAP_WITH_LIBS 
+
+#include <net/netmap_user.h>
+
+
+#define PROTO_IP		0x0800
+#define PROTO_ARP		0x0806
+
+
+#define PROTO_UDP		17
 
 
 
 #pragma pack(1)
 
-typedef unsigned int uint32;
+#define ETH_MAC_LENGTH		6
 
-#define ETH_MAX_LENGTH 6
+struct ethhdr {
 
-struct ethhdr{  //以太网头
-   unsigned char h_dest[ETH_MAX_LENGTH];  //目的地址
-   unsigned char h_source[ETH_MAX_LENGTH];//源地址
+	unsigned char h_dest[ETH_MAC_LENGTH];
+	unsigned char h_source[ETH_MAC_LENGTH];
+	unsigned short h_proto;
 
-   unsigned short h_proto;                //类型
+};
+
+
+struct iphdr {
+
+	unsigned char version;
+	unsigned char tos;
+	unsigned short length;
+	unsigned short id;
+	unsigned short flag_off;
+	unsigned char ttl;
+	unsigned char proto;
+	unsigned short check;
+
+	unsigned int saddr;
+	unsigned int daddr;
+
+	//unsigned char opt[0];
+
 };
 
 
+struct udphdr {
 
-
-struct iphdr{
-   unsigned  char  version;
-   unsigned char* tos;
-   unsigned short length;
-
-   unsigned short flag_off;
-
-   unsigned char ttl;
-   unsigned char proto;
-   unsigned short check;
-
-
-   unsigned int saddr;
-   unsigned int aaddr;
-};
-
-
-
-struct udphdr{
-   unsigned short sport;
-   unsigned short dport;
-   unsigned short len;
-   unsigned short check;
-};
-
-
-
-
-struct updpkt{
-    struct ethhdr eh;
-    struct iphdr ip;
-    struct udphdr udp;
-
-    unsigned char body[0];
+	unsigned short sport;
+	unsigned short dport;
+	unsigned short len;
+	unsigned short check;
 
 };
+
+struct udppkt {
+
+	struct ethhdr eh;
+	struct iphdr ip;
+	struct udphdr udp;
+
+	unsigned char body[0];
+
+};
+
 
 struct arphdr {
+
 	unsigned short h_type;
 	unsigned short h_proto;
 	unsigned char h_addrlen;
 	unsigned char protolen;
 	unsigned short oper;
-	unsigned char smac[ETH_ALEN];
+	unsigned char smac[ETH_MAC_LENGTH];
 	unsigned int sip;
-	unsigned char dmac[ETH_ALEN];
+	unsigned char dmac[ETH_MAC_LENGTH];
 	unsigned int dip;
+
+};
+
+struct arppkt {
+
+	struct ethhdr eh;
+	struct arphdr arp;
+
 };
 
 
 
-int main(){
+int str2mac(char *mac, char *str) {
 
-    //打开网卡设备
-    struct nm_desc* nmr=nm_open("net_map:eth0",NULL);
-    if(nmr==NULL){
-        return -1;
-    }
+	char *p = str;
+	unsigned char value = 0x0;
+	int i = 0;
 
-    //设置poll读事件
-    struct pollfd pfd={0};
-    pfd.fd=nmr->fd;
-    pfd.events=POLLIN;
+	while (p != '\0') {
+		
+		if (*p == ':') {
+			mac[i++] = value;
+			value = 0x0;
+		} else {
+			
+			unsigned char temp = *p;
+			if (temp <= '9' && temp >= '0') {
+				temp -= '0';
+			} else if (temp <= 'f' && temp >= 'a') {
+				temp -= 'a';
+				temp += 10;
+			} else if (temp <= 'F' && temp >= 'A') {
+				temp -= 'A';
+				temp += 10;
+			} else {	
+				break;
+			}
+			value <<= 4;
+			value |= temp;
+		}
+		p ++;
+	}
 
-    
-    unsigned char*stream=NULL;
-    struct nm_pkthdr h;
-    while(1){
-         
-        //poll监听
-        int ret=poll(&fd,1,-1);
-        if(ret<0) continue;
-        
-        //网卡来数据了
-        if(pfd.revents&POLLIN){
-          stream =nm_nextpkt(mnr,&h); //抓回数据
+	mac[i] = value;
 
-          //转为以太网帧格式
-          struct ethhdr*eh=(struct ethhd*)stream;
+	return 0;
+}
 
-          if(ntohs(eh->h_proto)==PROTO_IP){ //判断是不是IP数据
-              
-              //转为udp格式
-              struct updpkt *upd=(struct updpkt*)stream; 
+void echo_arp_pkt(struct arppkt *arp, struct arppkt *arp_rt, char *hmac) {
 
+	memcpy(arp_rt, arp, sizeof(struct arppkt));
 
-              if(ntohs(udp->ip.proto)==PROTO_UDP){ //判断是不是UDP数据
-                   int udp_length= ntohs(udp->udp_length);
+	memcpy(arp_rt->eh.h_dest, arp->eh.h_source, ETH_MAC_LENGTH);
+	str2mac(arp_rt->eh.h_source, hmac);
+	arp_rt->eh.h_proto = arp->eh.h_proto;
 
-                   udp->body[udp_length-8]='\0'; //截取
-
-                   printf("udp->%\n",udp->body);
-
-              }         
-          }
-        }
-        else if(ntohs(et->h_proto)==PROTO_ARP){  //判断是不是ARP数据
-             struct arppkt* arp=(struct arppkt*)stream;
-             struct arppkt arp_rt;
-
-             if(arp->arp.dip==inet_addr("192.168.2.217"))//判断是不是本地的地址
-             {
-                 
-             }
-
-
-        }
-
-    }
-
+	arp_rt->arp.h_addrlen = 6;
+	arp_rt->arp.protolen = 4;
+	arp_rt->arp.oper = htons(2);
+	
+	str2mac(arp_rt->arp.smac, hmac);
+	arp_rt->arp.sip = arp->arp.dip;
+	
+	memcpy(arp_rt->arp.dmac, arp->arp.smac, ETH_MAC_LENGTH);
+	arp_rt->arp.dip = arp->arp.sip;
 
 }
 
 
 
+int main() {
 
+	struct nm_desc *nmr = nm_open("netmap:eth0", NULL, 0, NULL);
+	if (nmr == NULL) {
+		return -1;
+	}
 
+	struct pollfd pfd = {0};
+	pfd.fd = nmr->fd;
+	pfd.events = POLLIN;
+
+	unsigned char *stream = NULL;
+	struct nm_pkthdr h;
+
+	while (1) {
+
+		int ret = poll(&pfd, 1, -1);
+		if (ret < 0) continue;
+
+		if (pfd.revents & POLLIN) {
+
+			stream = nm_nextpkt(nmr, &h);
+			struct ethhdr *eh = (struct ethhdr*)stream;
+
+			printf("proto: %x\n", ntohs(eh->h_proto));
+			if (ntohs(eh->h_proto) == PROTO_IP) {
+
+				struct udppkt *udp = (struct udppkt *)stream;
+				
+				printf("udp->ip.proto: %d\n", udp->ip.proto);
+				if (udp->ip.proto == PROTO_UDP) {
+
+					int udp_length = ntohs(udp->udp.len);
+
+					udp->body[udp_length-8] = '\0';
+					printf("udp-> %s\n", udp->body);
+				
+				}
+				
+			} else if (ntohs(eh->h_proto) == PROTO_ARP) {
+
+				struct arppkt *arp = (struct arppkt*)stream;
+				struct arppkt arp_rt;
+
+				if (arp->arp.dip == inet_addr("192.168.2.217")) {
+
+					echo_arp_pkt(arp, &arp_rt, "00:50:56:33:1c:ca");
+					nm_inject(nmr, &arp_rt, sizeof(struct arppkt));
+					
+				}
+				
+
+			}
+			
+		}
+
+	}
+
+}
 
 
 
